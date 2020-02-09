c5b2ce27fe0a0801d0b7f93d9516d40e0727311f
Fabrício Melo <fabriciomelosilva@gmail.com>
2020-02-09 12:16:41 -0300

    Montando estrutura do layout - menu e rodapé

--- public/app/themes/catolica/footer.php
+++ public/app/themes/catolica/footer.php
@@ -1,36 +1,109 @@
-<?php
-/**
- * The template for displaying the footer
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package catolica
- */
+<div class="footer-1">
 
-?>
+<section>
 
-	</div><!-- #content -->
+    <article class="col-25">
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'catolica' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'catolica' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'catolica' ), 'catolica', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+        <ul>
+            <li>Acadêmico</li>
+            <li><a href="#" target="_blank">Avaliação institucional</a></li>
+            <li><a href="#" target="_blank">Solicitar certificado de eventos</a></li>
+            <li><a href="#" target="_blank">Calendário acadêmico</a></li>
+        </ul>
+
+    </article>
+
+    <article class="col-25">
+
+        <ul>
+            <li>Documentos</li>
+            <li><a href="#" target="_blank">Bolsas e financiamentos</a></li>
+            <li><a href="#" target="_blank">Manual institucional</a></li>
+            <li><a href="#" target="_blank">Regulamentos institucionais</a></li>
+        </ul>
+
+    </article>
+
+    <article class="col-25">
+
+        <ul>
+            <li>Publicações</li>
+            <li><a href="#" target="_blank">Revista científica IES</a></li>
+            <li><a href="#" target="_blank">Revista científica dos cursos</a></li>
+            <li><a href="#" target="_blank">Acervo científico</a></li>
+        </ul>
+
+    </article>
+
+    <article class="col-25">
+
+        <ul>
+            <li>Links úteis</li>
+            <li><a href="#" target="_blank">Núcleo de Apoio Psicopedagógico</a></li>
+            <li><a href="#" target="_blank">Comitê de Ética em Pesquisa com seres humanos</a></li>
+            
+        </ul>
+
+    </article>
+
+</section>
+
+</div>
+
+<div class="footer-2">
+
+<section>
+
+    <article class="col-25">
+
+        <img src="assets/img/logo-2.png" alt="Logotipo antigo">
+
+    </article>
+
+    <article class="col-50">
+
+        <p>Rua Tenente Benévolo, 201 - Centro | Fortaleza, Ceará - Brasil</p>
+        <a href="mailto:catolicafort@catolicadefortaleza.edu.br">catolicafort@catolicadefortaleza.edu.br</a>
+        <p>(85) 3453.2150 / (85) 99690.0302</p>
+
+    </article>
+
+    <article class="col-25">
+
+        <ul>
+            <li>Contato
+                <ul class="social">
+                    <li><a href="#"><img src="assets/img/icone-wpp.png" alt=""></a> <a href="#"><img src="assets/img/icone-face.png" alt=""></a></li>
+                </ul>
+            </li>
+            <li><a href="modal/trabalhe.php" rel="modal:open">Trabalhe conosco</a></li>
+            <li><a href="modal/contato.php" rel="modal:open">Fale conosco</a></li>
+        </ul>
+
+    </article>
+
+</section>
+
+</div>
+
+
+<footer>
+
+2019 - Faculdade Católica de Fortaleza. Todos os direitos reservados
+
+</footer>
 
 <?php wp_footer(); ?>
 
+<div class="chat">
+<a href="#"><img src="assets/img/chat.png" alt="Chat"></a>
+</div>
+
+<?php echo '<script type="text/javascript" src="' .get_stylesheet_directory_uri()  . '/assets/js/jquery.js">' ?>
+<?php echo '<script type="text/javascript" src="' .get_stylesheet_directory_uri()  . '/assets/js/slick.min.js">' ?>
+<?php echo '<script type="text/javascript" src="' .get_stylesheet_directory_uri()  . '/assets/js/modal.js">' ?>
+<?php echo '<script type="text/javascript" src="' .get_stylesheet_directory_uri()  . '/assets/js/funcoes.js">' ?>
+
 </body>
+
 </html>
