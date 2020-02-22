9792df884ebb6d1986d8072de99a923e03e2cba3
Fabrício Melo <fabriciomelosilva@gmail.com>
2020-02-22 13:02:13 -0300

    Ajustes no codigo

--- public/app/themes/catolica/index.php
+++ public/app/themes/catolica/index.php
@@ -1,23 +1,19 @@
 <?php // Template Name: Faculdade Católica ?>
 
-<?php
+<?php get_header(); ?>
+<?php get_template_part('sections/nav', 'index'); ?>
 
-get_header();
-
-		get_template_part('sections/nav', 'index'); ?>
-
-		<div class="banner-home">
-			<?php while(the_repeater_field('galeria-home')): ?>
-			<?php $image = get_sub_field('img'); ?>
-				<div><img src="<?php echo $image; ?>"> </div>
-			<?php endwhile; ?>
-		</div>
+	<div class="banner-home">
+		<?php while(the_repeater_field('galeria-home')): ?>
+		<?php $image = get_sub_field('img'); ?>
+			<div><img src="<?php echo $image; ?>"> </div>
+		<?php endwhile; ?>
+	</div>
 
 <?php 
-		get_template_part('sections/nav', 'news'); 
-		get_template_part('sections/nav', 'events'); 
-		get_template_part('sections/nav', 'conheca'); 
+	get_template_part('sections/nav', 'news'); 
+	get_template_part('sections/nav', 'events'); 
+	get_template_part('sections/nav', 'conheca'); 
+?>
 
-
-
-get_footer(); ?>
+<?php get_footer(); ?>
